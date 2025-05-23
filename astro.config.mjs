8db04517b195c4d76f6f3784ab50464831e6468b
dynivthuriaf <bertrandcoltrande@gmail.com>
2025-05-23 09:39:26 +0200

    Ajoutes moi un message de commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://dynivthuriaf.github.io',
+  base:'/cv_portfolio_Dyniv_Ioan/',
+  trailingSlash:'always',
   integrations: [tailwind()],
 });
